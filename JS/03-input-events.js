cef3403a70a00090f12ed60b30c26c6e6fc51fb5
AndreyHerzhov <andreyherzhov@gmail.com>
2022-05-10 21:11:25 +0300

    multiplyLi

--- JS/03-input-events.js
+++ JS/03-input-events.js
@@ -20,25 +20,25 @@ const refs = {
 // refs.input.addEventListener('input', onInputChange)
 
 refs.input.addEventListener('input', onInputChange)
-refs.licenseCheckbox.addEventListener('change', onLicenseChange)
+// refs.licenseCheckbox.addEventListener('change', onLicenseChange)
 
-function onInputBlur() {
-    console.log('Инпут потерял фокус - событие blur')
-}
+// function onInputBlur() {
+//     console.log('Инпут потерял фокус - событие blur')
+// }
 
-function onInputFocus() {
-    console.log('Инпут получил фокус - событие focus')
-}
+// function onInputFocus() {
+//     console.log('Инпут получил фокус - событие focus')
+// }
 
 function onInputChange() {
     console.log(event.currentTarget.value)
     refs.nameLabel.textContent = event.currentTarget.value;
 }
 
-function onLicenseChange() {
-    console.log(event.currentTarget.checked)
-    refs.btn.disabled = !event.currentTarget.checked
-}
+// function onLicenseChange() {
+//     console.log(event.currentTarget.checked)
+//     refs.btn.disabled = !event.currentTarget.checked
+// }
 
 // const input = document.querySelector('.js-input')
 // const nameLabel = document.querySelector('.js-button > span')
---
+++ JS/07-multiplyElement.js
@@ -0,0 +1,11 @@
+const list = document.querySelector('.list')
+
+const array = ['HTML', 'CSS', 'JavaScript', 'Node.js', 'ReactNative']
+
+const createList = function(text) {
+    const li = document.createElement('li')
+    li.textContent = text
+    list.append(li)
+}
+
+array.map(el => createList(el))
